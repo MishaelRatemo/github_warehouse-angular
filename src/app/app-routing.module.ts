d61bcfb24159eaccdbfb4b027e08cbccaabbe5cb
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-14 18:22:43 +0300

    Added user details route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { UserdetailsComponent } from './components/userdetails/userdetails.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  { path: 'search-results/:query', component: SearchResultsComponent}
+  { path: 'search-results/:query', component: SearchResultsComponent},
+  { path: 'userdetails/:id', component: UserdetailsComponent}
 ];
 
 @NgModule({

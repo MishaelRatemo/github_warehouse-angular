fd750f7cba623541cddf407002dd3470c42c8e05
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-16 21:16:41 +0300

    Added getreposResusults method

--- src/app/services/github-users.service.ts
+++ src/app/services/github-users.service.ts
@@ -1,18 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubUsersService {
+   header = {
+    headers: new HttpHeaders()
+      .set('Authorization',  `Bearer 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`)
+  }
+  
 
   //  private searchText ='';
+   private token = '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';
 
   constructor(private http:HttpClient) { }
 
   public getSearchResults(searchText:any){
     return new Promise((resolve, reject) =>{
-      this.http.get(`https://api.github.com/users/${searchText}`).subscribe(
+      this.http.get(`https://api.github.com/users/${searchText}`,this.header).subscribe(
         (response)=>{
           resolve(response);
         },
@@ -22,4 +28,34 @@ export class GithubUsersService {
       );
     })
   }
+
+  public getReposResult(searchText:any){
+    return new Promise((resolve, reject) =>{
+      this.http.get(`https://api.github.com/search/repositories?q=${searchText}`,this.header).subscribe(
+        (response)=>{
+          resolve(response);
+        },
+        (error)=>{
+          reject(error);
+        }
+      );
+    })
+  }
+
+
+  public getUserDetails(username:any,id:any){
+    return new Promise((resolve, reject) =>{
+      this.http.get(`https://api.github.com/users/${username}/${id}`).subscribe(
+        (res)=> {
+          resolve(res);
+        },
+        (err) =>{
+          reject(err);
+        }
+      );
+    })
+  }
+
+
+
 }

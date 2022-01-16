fd008c53c4b7c2c248768fb57d42566e110a6cbb
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-16 20:12:06 +0300

    made some updates on the pipe

--- src/app/pipes/timepassed.pipe.ts
+++ src/app/pipes/timepassed.pipe.ts
@@ -6,28 +6,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimepassedPipe implements PipeTransform {
 
   transform(value: any): any {
-    let counter:any;
-    let currentTime:any = new Date();
+    let currentTime: any = new Date();
     const second = 1000;
-    var timeDiff = Math.abs(value - currentTime)/second;
+    let counter: any;
 
-    let timeInterval:any = { // in seconds
-      'secs':1,
-      'mins':60*1,
-      'hrs':60*60,
-      'days':60*60*24,
-      'weeks':60*60*24*7,
-      'months':60*60*24*30,// Assume every month has 30 days
-      'years':60*60*24*365,// a yeah has 365 days
-    }
-     for(let i in timeInterval){
-       counter = Math.floor( timeDiff/timeInterval);
-        if (counter > 0){
-          return `${counter} ${i} ago`
+    let timeInterval: any = { // in seconds
+      'years': 60 * 60 * 24 * 365,// a yeah has 365 days
+      'months': 60 * 60 * 24 * 30,// Assume every month has 30 days 
+      'weeks': 60 * 60 * 24 * 7,
+      'days': 60 * 60 * 24,
+      'hrs': 60 * 60,
+      'mins': 60 * 1,
+      'secs': 1
+    };
+
+    if (value) {
+      let timeDiff = Math.abs(currentTime - +new Date(value)) / second;
+      if (timeDiff < 10)
+        return 'Just created now'
+      for (let i in timeInterval) {
+        counter = Math.floor(timeDiff / timeInterval[i]);
+        if (counter > 0) {
+          return counter + ' ' + i + ' ' + 'ago';
         }
-     }
-
+      }
+    }
     return value;
   }
-
 }
+

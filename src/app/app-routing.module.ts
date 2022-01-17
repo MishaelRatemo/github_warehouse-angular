286a8b4438d842b94023ba22c56f2c5b12eaa46e
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-17 19:35:26 +0300

    added repos, userdetailsa ns repositories routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { UserdetailsComponent } from './components/userdetails/userdetails.component';
 
@@ -8,7 +10,10 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   { path: 'search-results/:query', component: SearchResultsComponent},
-  { path: 'userdetails/:id', component: UserdetailsComponent}
+  { path: 'userdetails/:id', component: UserdetailsComponent},
+  { path:'repos', component:ReposComponent },
+  { path: 'repositories/:query', component: RepositoriesComponent}
+
 ];
 
 @NgModule({
